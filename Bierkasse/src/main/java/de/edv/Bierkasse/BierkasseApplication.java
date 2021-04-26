1d1ff78b5b19b45a24a6eb43a280aae56a805914
flwerner <flwerner>
2021-04-26 16:28:28 +0200

    Generate Spring

---
+++ Bierkasse/src/main/java/de/edv/Bierkasse/BierkasseApplication.java
@@ -0,0 +1,13 @@
+package de.edv.Bierkasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BierkasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BierkasseApplication.class, args);
+	}
+
+}
---
+++ Bierkasse/src/test/java/de/edv/Bierkasse/BierkasseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.edv.Bierkasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BierkasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
