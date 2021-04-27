7faff04442269e1169e91d6b96f82c1ae81dca4f
Markus <48181660+BackInBash@users.noreply.github.com>
2021-04-27 11:41:43 +0200

    Fixed Syntax

--- Bierkasse/src/main/java/de/edv/Bierkasse/Controller/BuchungController.java
+++ Bierkasse/src/main/java/de/edv/Bierkasse/Controller/BuchungController.java
@@ -7,6 +7,7 @@ package de.edv.Bierkasse.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import test.person;
 
@@ -41,7 +44,7 @@ public class BuchungController {
         initialize();
         person[] tmp = new person[personen.size()];
         return personen.toArray(tmp);
-        Repository.
+        //Repository.
     }
     
 //    @GetMapping("/create")
