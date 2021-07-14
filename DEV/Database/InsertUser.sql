ecaf95b7fe24570e94d20c768cbcd03ee9f85b13
flwerner <flwerner>
2021-07-14 12:11:37 +0200

    Update Folder

---
+++ DEV/Database/InsertUser.sql
@@ -0,0 +1,9 @@
+#RFID-Name-Vorname-Guthaben-istAdmin
+INSERT INTO benutzer
+VALUES (187, Werner, Florian, 10, 1)
+VALUES (188, Mayer, Julian, 10, 0)
+VALUES (189, Kost, Marvin, 10, 0) 
+VALUES (190, Tremmel, Jonas, 10, 0)
+VALUES (191, Bertsch, Robert, 10, 0)
+VALUES (192, Büchele, Mika, 10, 0)
+
