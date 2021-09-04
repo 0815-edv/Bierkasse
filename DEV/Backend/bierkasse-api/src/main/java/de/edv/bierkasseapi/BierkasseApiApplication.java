3ffc28f58394579036dc2572585ddb36bf21c6e9
flwerner <76451227+TheRealRanger@users.noreply.github.com>
2021-09-04 15:33:22 +0200

    NEW RESTAPI-SQLProxy

---
+++ DEV/Backend/bierkasse-api/src/main/java/de/edv/bierkasseapi/BierkasseApiApplication.java
@@ -0,0 +1,13 @@
+package de.edv.bierkasseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BierkasseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BierkasseApiApplication.class, args);
+	}
+
+}
---
+++ DEV/Backend/bierkasse-api/src/test/java/de/edv/bierkasseapi/BierkasseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.edv.bierkasseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BierkasseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
